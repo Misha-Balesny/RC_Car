86d98df496b09ca240383b9edc801ef92c66c00c
Babulesny <misha271000@gmail.com>
2019-11-03 19:38:14 +0300

    commit

---
+++ RX/RX.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include <Servo.h>
+
+RF24 radio(9,10); 
+Servo servo;
+
+byte address[][6] = {"1Node","2Node","3Node","4Node","5Node","6Node"};  
+byte gotPack[2], DirX, DirY, Forward, Backward; 
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  servo.attach(8);
+  radio.begin();  
+  radio.setAutoAck(0);         
+  radio.setRetries(0,15);     
+  radio.enableAckPayload();    
+  radio.setPayloadSize(2);     
+  radio.openReadingPipe(1,address[0]);      
+  radio.setChannel(0x66);  
+  radio.setPALevel (RF24_PA_MAX);
+  radio.setDataRate (RF24_250KBPS);
+  radio.powerUp(); 
+  radio.startListening(); 
+  pinMode(7, OUTPUT);
+  pinMode(4, OUTPUT); 
+}
+
+void loop() 
+{
+    byte pipeNo;                         
+    while( radio.available(&pipeNo))
+      radio.read(&gotPack, sizeof(gotPack));
+    DirX = gotPack[0];
+    DirY = gotPack[1];
+    if (DirX < 132)
+      Backward = map(DirX, 0, 131, 255, 0);
+    else
+      Forward = map(DirX, 132, 255, 0, 255);
+    if (Forward > 0)
+      Backward = 0;
+    analogWrite(7, Forward);
+    analogWrite(4, Backward);
+    Serial.print(Forward);
+    Serial.print("  "); 
+    Serial.print(Backward);
+    Serial.print("  ");
+    Serial.println(DirY);
+    servo.write(DirY);     
+}
---
+++ TX/TX.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>          
+#include "nRF24L01.h"     
+#include "RF24.h"       
+
+RF24 radio(9, 10); 
+
+byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
+byte Pack[2];
+
+void setup() {
+  Serial.begin(9600); 
+  
+  radio.begin(); 
+  radio.setAutoAck(0);        
+  radio.setRetries(0, 15);    
+  radio.enableAckPayload();   
+  radio.setPayloadSize(2);  
+  radio.openWritingPipe(address[0]); 
+  radio.setChannel(0x66); 
+  radio.setPALevel (RF24_PA_MAX); 
+  radio.setDataRate (RF24_250KBPS); 
+  radio.powerUp();
+  radio.stopListening(); 
+}
+
+void loop() {
+
+  Pack[0] = map(analogRead(0), 0, 1023, 0, 255);
+  Pack[1] = map(analogRead(1), 0, 1023, 90-45, 90+45);
+  radio.write(&Pack, sizeof(Pack));
+  Serial.print(Pack[0]);
+  Serial.print("  ");
+  Serial.println(Pack[1]); 
+
+}
