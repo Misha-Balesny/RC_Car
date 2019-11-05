bfa957ebd3641920d3383b83d2116873af5f7727
Babulesny <misha271000@gmail.com>
2019-11-05 13:06:59 +0300

    2

--- TX/TX.ino
+++ TX/TX.ino
@@ -7,9 +7,8 @@ RF24 radio(9, 10);
 byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //возможные номера труб
 byte Pack[2];
 
-void setup() {
-  Serial.begin(9600); 
-  
+void setup() 
+{
   radio.begin(); 
   radio.setAutoAck(0);        
   radio.setRetries(0, 15);    
@@ -23,13 +22,12 @@ void setup() {
   radio.stopListening(); 
 }
 
-void loop() {
-
+void loop() 
+{
   Pack[0] = map(analogRead(0), 0, 1023, 0, 255);
   Pack[1] = map(analogRead(1), 0, 1023, 90-45, 90+45);
   radio.write(&Pack, sizeof(Pack));
   Serial.print(Pack[0]);
   Serial.print("  ");
   Serial.println(Pack[1]); 
-
 }
